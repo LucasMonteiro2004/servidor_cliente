6d1ded609b6358d78b64998e2ecf25d4b00b5b31
lucas <lucas@Ubuntu.myguest.virtualbox.org>
2023-11-13 12:51:09 +0000

    first commit

---
+++ eco.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+#include "util.h"
+
+
+int main(int argc, char *argv[]) {
+    int fd, nbytes;
+    char str[20];
+
+    mkfifo(PIPE_NAME, 0644);
+    printf("Criei FIFO... \n");
+    fd = open(PIPE_NAME, O_RDONLY);
+    printf("Abri FIFO (READ)... \n");
+    do{
+        nbytes = read(fd, str, sizeof(str) -1);
+        if(nbytes > 0){
+            str[nbytes] = '\0';
+            printf("Li...'%s (%d bytes)'\n", str, nbytes);
+
+        }
+    }while (nbytes > 0);
+    close(fd);
+    printf("fechei FIFO... \n");
+    unlink(PIPE_NAME);
+    printf("Removi FIFO... \n");
+
+    return 0;
+}
---
+++ util.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+#define PIPE_NAME "meu_pipe"
