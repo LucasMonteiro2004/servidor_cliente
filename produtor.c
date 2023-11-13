421b366d341afee1e383ef3a9e96bc4f8b4fe8bf
lucas <lucas@Ubuntu.myguest.virtualbox.org>
2023-11-13 13:10:26 +0000

    first commit

--- produtor.c
+++ produtor.c
@@ -14,8 +14,9 @@ int main(int argc, char *argv[]){
     if(access(PIPE_NAME, F_OK) != 0){
         printf("[ERRO] FIFO '%s' nao existe", PIPE_NAME);
     }
-    printf("Verifiquei FIFO... '%s", PIPE_NAME);
+    printf("Verifiquei FIFO... '%s'\n", PIPE_NAME);
     fd = open(PIPE_NAME, O_WRONLY);
+    printf("Abri FIFO (WRITE)... '%s'\n", PIPE_NAME);
     do{
         scanf("%s", str);
         nbytes = read(fd, str, sizeof(str) -1);
